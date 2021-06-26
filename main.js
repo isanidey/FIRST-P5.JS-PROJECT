44e58d7014e104786be3bf54ccac35d5176c1056
Isani Love <74708250+isanidey@users.noreply.github.com>
2021-06-26 13:14:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function preload(){
+}
+function setup()
+{
+    canvas= createCanvas(640, 480);
+    canvas.position(110, 250);
+    video= createCapture(VIDEO);
+video.hide();
+tint_color= "";
+}
